d8ee0b5467c82e392ae9d5ba03c8c35c3e19784e
apprexp <1597133323@qq.com>
2025-02-24 00:05:17 +0800

    second commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:As1597133323@rm-cn-fxf459tpy0003foo.rwlb.rds.aliyuncs.com:3306/carousell'  # 换成你的数据库信息
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://palk:Admin123@rm-cn-fxf459tpy0003foo.rwlb.rds.aliyuncs.com:3306/carousell'  # 换成你的数据库信息
     SQLALCHEMY_COMMIT_ON_TEARDOWN= True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     db = None
