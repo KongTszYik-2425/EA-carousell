4bc29b51aae6bcfcddc69c0ad25a777ab33a9eb3
KongTszYik-2425 <240070125@stu.vtc.edu.hk>
2025-03-25 03:57:56 +0000

    aa

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from application import app
 if __name__ == '__main__':
     # app.run(host, port, debug, options)
     # 默认值：host="127.0.0.1", port=5000, debug=False
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000,debug=True)
